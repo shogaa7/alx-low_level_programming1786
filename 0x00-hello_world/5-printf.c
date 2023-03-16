bed229ec91f71d5d0682b358d8521ab459da355c
shogaa7 <mahmoudshogaa71@gmail.com>
2023-03-16 16:27:41 -0700

    nytb

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - file name 5-printf.c
+  *Return: Always 0.
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
