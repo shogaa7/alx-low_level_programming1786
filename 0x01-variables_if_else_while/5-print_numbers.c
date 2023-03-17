0cb18e4caff9cb91939e80ae8df4b985fd863549
shogaa7 <mahmoudshogaa71@gmail.com>
2023-03-17 09:46:10 -0700

    hh

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - jajaj
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+char ch = '0';
+for (ch = '0'; ch <= '9'; ch++)
+
+putchar(ch);
+
+putchar('\n');
+
+return (0);
+}
